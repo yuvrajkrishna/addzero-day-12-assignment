07be36a42d7708aa1f01042e93007fad68722b13
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-21 21:11:19 +0530

    seven

---
+++ seven.java
@@ -0,0 +1,10 @@
+public class seven {
+    public static void main(String[] args) {
+        int arr [] = {10,20,30,40,50};
+        int n = arr.length;
+        int mth = 2;
+        if(mth >= 0 && mth < arr.length){
+            System.out.println("mth element is  " + arr[mth-1]);
+        }
+    }
+}
--- six.java
+++ six.java
@@ -3,7 +3,7 @@ public class six {
         int [] arr = new int [] {10,20,30};
         int mth = 2;
         if (mth >= 0 && mth < arr.length) {
-            System.out.println("The Mth Element Exists and its value is : " + arr[mth]);
+            System.out.println("The Mth Element Exists and its value is : " + arr[mth-1]);
         } 
         else {
             System.out.println("The Mth Element does not exist");
