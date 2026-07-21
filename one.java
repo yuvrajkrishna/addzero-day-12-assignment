b622b4c4ef9c2faaa8bcd370c99dbfaf79538759
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-21 20:37:16 +0530

    one

---
+++ one.java
@@ -0,0 +1,10 @@
+public class one {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+        int n = arr.length;
+        for(int i = 0 ; i < n; i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
