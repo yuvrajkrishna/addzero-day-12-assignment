48c9aae89442112a9f5503fdf14201c0eb984349
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-21 20:39:33 +0530

    two

---
+++ two.java
@@ -0,0 +1,9 @@
+public class two {
+    public static void main (String args []){
+        int arr [] = {2,4,6,8,10};
+        int n = arr.length;
+        for(int i = n - 1; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
