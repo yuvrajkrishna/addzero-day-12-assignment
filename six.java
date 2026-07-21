8eb27c28facafea84ed6d475a4b5b5a08d8404e3
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-21 21:02:09 +0530

    it also works with if else

--- six.java
+++ six.java
@@ -2,19 +2,11 @@ public class six {
     public static void main(String[] args) {
         int [] arr = new int [] {10,20,30};
         int mth = 2;
-        int n = arr.length;
-        int check = 0;
-        for(int i = 0 ; i < n; i++){
-            if(i == mth){
-                check  = 1;
-            }
-        }
-        if(check == 1){
+        if (mth >= 0 && mth < arr.length) {
             System.out.println("The Mth Element Exists and its value is : " + arr[mth]);
-        }
-
-        else{
-            System.out.println("The Mth Element is not Exists");
+        } 
+        else {
+            System.out.println("The Mth Element does not exist");
         }
 
         }
